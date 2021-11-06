fcc8dddb4719757bab8f7d0c24251efeb09c753b
Sheehan Olver <solver@mac.com>
2021-11-06 20:36:30 +0000

    Package

---
+++ src/ColorBitstring.jl
@@ -0,0 +1,30 @@
+module ColorBitstring
+export printbits
+
+printred(io::IO, x) = print(io, "\x1b[31m" * x * "\x1b[0m")
+printgreen(io::IO, x) = print(io, "\x1b[32m" * x * "\x1b[0m")
+printblue(io::IO, x) = print(io, "\x1b[34m" * x * "\x1b[0m")
+
+function printbits(io::IO, x::Float16)
+       bts = bitstring(x)
+       printred(io, bts[1:1])
+       printgreen(io, bts[2:7])
+       printblue(io, bts[8:end])
+end
+
+function printbits(io::IO, x::Float32)
+   bts = bitstring(x)
+    printred(io, bts[1:1])
+    printgreen(io, bts[2:2+8-1])
+    printblue(io, bts[2+8:end])
+end
+
+function printbits(io::IO, x::Float64)
+   bts = bitstring(x)
+    printred(io, bts[1:1])
+    printgreen(io, bts[2:2+11-1])
+    printblue(io, bts[2+11:end])
+end
+
+printbits(x) = printbits(stdout, x)
+end
---
+++ test/runtests.jl
@@ -0,0 +1,11 @@
+using ColorBitstring, Test
+
+@testset "ColorBitstring" begin
+    io = IOBuffer()
+    printbits(io, Float16(1))
+    @test String(take!(io)) == "\e[31m0\e[0m\e[32m011110\e[0m\e[34m000000000\e[0m"
+    printbits(io, Float32(1))
+    @test String(take!(io)) == "\e[31m0\e[0m\e[32m01111111\e[0m\e[34m00000000000000000000000\e[0m"
+    printbits(io, 1.0)
+    @test String(take!(io)) == "\e[31m0\e[0m\e[32m01111111111\e[0m\e[34m0000000000000000000000000000000000000000000000000000\e[0m"
+end
