c113ba588a8c449a53b35c57631ad7aba637a56d
Sheehan Olver <solver@mac.com>
2021-12-10 16:05:05 +0000

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,11 +3,10 @@ using ColorBitstring, Test
 @testset "ColorBitstring" begin
     io = IOBuffer()
     printbits(io, Float16(1))
-    @test String(take!(io)) == "\e[31m0\e[0m\e[32m011110\e[0m\e[34m000000000\e[0m"
+    @test String(take!(io)) == "\e[31m0\e[0m\e[32m01111\e[0m\e[34m0000000000\e[0m"
     printbits(io, Float32(1))
     @test String(take!(io)) == "\e[31m0\e[0m\e[32m01111111\e[0m\e[34m00000000000000000000000\e[0m"
     printbits(io, 1.0)
     @test String(take!(io)) == "\e[31m0\e[0m\e[32m01111111111\e[0m\e[34m0000000000000000000000000000000000000000000000000000\e[0m"
-    printbits(io, 1)
-    
+    printbits(io, 1) 
 end
