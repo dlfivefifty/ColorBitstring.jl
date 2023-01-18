cd253977fad2dc4d526d258698e59dd4efaf1148
Sheehan Olver <solver@mac.com>
2023-01-18 15:44:21 +0000

    Add parsebinary and binarystring (#2)
    * Add parsebinary and binarystring
    
    * Update ci.yml

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,5 +8,39 @@ using ColorBitstring, Test
     @test String(take!(io)) == "\e[31m0\e[0m\e[32m01111111\e[0m\e[34m00000000000000000000000\e[0m"
     printbits(io, 1.0)
     @test String(take!(io)) == "\e[31m0\e[0m\e[32m01111111111\e[0m\e[34m0000000000000000000000000000000000000000000000000000\e[0m"
-    printbits(io, 1) 
+    printbits(io, 1)
+end
+
+@testset "binarystring" begin
+    @test binarystring(2) == "(10)_2"
+    @test binarystring(-2) == "-(10)_2"
+    @test binarystring(2.0) == "2^1 * (1.0000000000000000000000000000000000000000000000000000)_2"
+    @test binarystring(Float16(2)) == "2^1 * (1.0000000000)_2"
+    @test binarystring(-2.0) == "-2^1 * (1.0000000000000000000000000000000000000000000000000000)_2"
+    @test binarystring(-Float16(2)) == "-2^1 * (1.0000000000)_2"
+
+    @test binarystring(1/2.0) == "2^-1 * (1.0000000000000000000000000000000000000000000000000000)_2"
+    @test binarystring(1/Float16(2)) == "2^-1 * (1.0000000000)_2"
+    @test binarystring(-1/2.0) == "-2^-1 * (1.0000000000000000000000000000000000000000000000000000)_2"
+    @test binarystring(-1/Float16(2)) == "-2^-1 * (1.0000000000)_2"
+end
+
+@testset "parsebinary" begin
+    @test parsebinary(Float64, "1") ≡ 1.0
+    @test parsebinary(Float32, "1") ≡ 1.0f0
+    @test parsebinary(Float64, "-1") ≡ -1.0
+    @test parsebinary(Float32, "-1") ≡ -1.0f0
+    @test parsebinary(Float64, "10") ≡ 2.0
+    @test parsebinary(Float32, "10") ≡ 2.0f0
+    @test parsebinary(Float64, "-10") ≡ -2.0
+    @test parsebinary(Float32, "-10") ≡ -2.0f0    
+    @test parsebinary(Float32, "1.") ≡ 1.0f0
+    @test parsebinary(Float32, "1.") ≡ 1.0f0
+    @test parsebinary(Float64, "1.001") ≡ 1.125
+    @test parsebinary(Float32, "1.001") ≡ 1.125f0
+    @test parsebinary(Float64, "10.001") ≡ 2.125
+    @test parsebinary(Float32, "10.001") ≡ 2.125f0
+
+    @test parsebinary(Float64, ".001") ≡ parsebinary(Float64, "0.001") ≡ 0.125
+    @test parsebinary(Float64, "-.001") ≡ parsebinary(Float64, "-0.001") ≡ -0.125
 end
