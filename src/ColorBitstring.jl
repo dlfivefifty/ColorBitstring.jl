3b59afe24c509975cba3323cf8a1def04110bad1
Sheehan Olver <solver@mac.com>
2023-01-18 15:31:09 +0000

    Add parsebinary and binarystring

--- src/ColorBitstring.jl
+++ src/ColorBitstring.jl
@@ -1,5 +1,5 @@
 module ColorBitstring
-export printbits, printlnbits
+export printbits, printlnbits, binarystring, parsebinary
 
 printred(io::IO, x) = print(io, "\x1b[31m" * x * "\x1b[0m")
 printgreen(io::IO, x) = print(io, "\x1b[32m" * x * "\x1b[0m")
@@ -39,4 +39,44 @@ function printlnbits(x)
     printbits(x)
     println()
 end
+
+
+# routines for other binary
+binarystring(x) = (signbit(x) ? "-" : "") * "(" * string(abs(x); base=2) * ")_2"
+
+function binarystring(x::AbstractFloat)
+    S = precision(x)-1
+    j = exponent(x)
+    str = string(BigInt(big(2)^S * significand(abs(x))); base=2)
+    str =     "2^$j * (" * str[1] * "." * str[2:end] * ")_2"
+    s_str = if x ≥ 0
+        ""
+    else
+        "-"
+    end
+    s_str * str
+end
+
+function binarystring(x::AbstractIrrational)
+    str = binarystring(big(x))
+    
+    str[1:end-3] * "…" * ")_2"
+end
+
+function parsebinary(T, str::String)
+    if str[1] == '-'
+        s = -1
+        str = str[2:end]
+    else
+        s = 1
+    end
+    j = findfirst(isequal('.'), str)
+    if isnothing(j)
+        s*convert(T, parse(BigInt, str; base=2))
+    else
+        intpart = parse(BigInt, str[1:j-1] * str[j+1:end]; base=2)
+        convert(T, s * intpart/(big(2)^(length(str)-j)))
+    end
+end
+
 end
